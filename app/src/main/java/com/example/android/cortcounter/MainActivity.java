c7153367f1f1c40e32cc7bd7ef329b2e2a44fe0d
Milrom <ra.main.w@gmail.com>
2020-02-27 12:46:11 +0300

    Orange

--- app/src/main/java/com/example/android/cortcounter/MainActivity.java
+++ app/src/main/java/com/example/android/cortcounter/MainActivity.java
@@ -8,9 +8,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    final int POINTS_FOR_FREE_THROW = 1;
+    final int PLUS_3_POINTS = 3;
+    final int PLUS_2_POINTS = 2;
+
     // Team A and Team B score counters
     int team_A_Counter = 0;
     int team_B_Counter = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Display methods
+
     private void display_A(int number){
         TextView score = findViewById(R.id.score_A_Text_View);
         score.setText("" + number);
@@ -29,14 +35,15 @@ public class MainActivity extends AppCompatActivity {
         score.setText("" + number);
     }
 
-    // Team A buttoms
+    //Team A buttoms
+
     public void plus_3_Points_A(View view) {
-        team_A_Counter = team_A_Counter + 3;
+        team_A_Counter = team_A_Counter + PLUS_3_POINTS;
         display_A(team_A_Counter);
     }
 
     public void plus_2_Points_A(View view) {
-        team_A_Counter = team_A_Counter + 2;
+        team_A_Counter = team_A_Counter + PLUS_2_POINTS;
         display_A(team_A_Counter);
     }
 
@@ -47,12 +54,12 @@ public class MainActivity extends AppCompatActivity {
 
     // Team B buttoms
     public void plus_3_Points_B(View view) {
-        team_B_Counter = team_B_Counter + 3;
+        team_B_Counter = team_B_Counter + PLUS_3_POINTS;
         display_B(team_B_Counter);
     }
 
     public void plus_2_Points_B(View view) {
-        team_B_Counter = team_B_Counter + 2;
+        team_B_Counter = team_B_Counter + PLUS_2_POINTS;
         display_B(team_B_Counter);
     }
 
@@ -67,5 +74,6 @@ public class MainActivity extends AppCompatActivity {
         team_B_Counter = 0;
         display_A(team_A_Counter);
         display_B(team_B_Counter);
+
     }
 }
